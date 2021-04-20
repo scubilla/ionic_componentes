5e7b31e2dbf545430a3f6aa0de034cdb4da98a59
scubilla <simoncubilla76@gmail.com>
2021-04-20 15:59:50 -0400

    hasta boton fab

--- src/app/pages/inicio/inicio.page.ts
+++ src/app/pages/inicio/inicio.page.ts
@@ -30,7 +30,33 @@ export class InicioPage implements OnInit {
     icon: 'beaker-outline',
     name: 'Avatar',
     redirectTo: '/avatar'
+   },
+   {
+    icon: 'radio-button-off-outline',
+    name: 'Buttons',
+    redirectTo: '/button'
+   },
+   {
+    icon: 'card-outline',
+    name: 'Cards',
+    redirectTo: '/card'
+   },
+   {
+    icon: 'checkmark-circle-outline',
+    name: 'Checks',
+    redirectTo: '/check'
+   },
+   {
+    icon: 'calendar-outline',
+    name: 'DateTime',
+    redirectTo: '/date-time'
+   },
+   {
+    icon: 'car-outline',
+    name: 'Fab',
+    redirectTo: '/fab'
    }
+   
   ];
 
   constructor() { }
